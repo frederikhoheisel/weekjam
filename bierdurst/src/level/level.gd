ca3b9ced7531c154a2e8263424fe44248796ff3d
HansLaptop <hans.kraschewski@mailbox.tu-dresden.de>
2026-05-26 18:11:24 +0200

    level verwirrung

---
+++ bierdurst/src/level/level.gd
@@ -0,0 +1,23 @@
+extends Node3D
+class_name Level
+
+@export var lvl_idx: int
+@export var player: CharacterBody3D
+@export var grid_map: GridMap
+
+var player_grid_pos: Vector2i
+var grid: Array[bool] = []
+# Called when the node enters the scene tree for the first time.
+
+func _ready() -> void:
+	player_grid_pos = 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("DOWN"):
+		if check_tile(player_grid_pos + Vector2i(0, 1)):
+			player_grid_pos += Vector2i(0, 1)
+	
+func check_tile(pos: Vector2i):
+	if grid[pos.x + ]
