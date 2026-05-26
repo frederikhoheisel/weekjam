6cf3605b0d03f108ac443929fe49653a861f4092
hanskraschewski <hans.kraschewski@mailbox.tu-dresden.de>
2026-05-26 23:11:18 +0200

    idk

--- bierdurst/src/level/level.gd
+++ bierdurst/src/level/level.gd
@@ -5,19 +5,19 @@ class_name Level
 @export var player: CharacterBody3D
 @export var grid_map: GridMap
 
-var player_grid_pos: Vector2i
+var player_grid_pos: Vector3
 var grid: Array[bool] = []
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
-	player_grid_pos = 
+	player_grid_pos = grid_map.local_to_map(grid_map.to_local(player.global_position))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("DOWN"):
-		if check_tile(player_grid_pos + Vector2i(0, 1)):
-			player_grid_pos += Vector2i(0, 1)
+		if check_tile(player_grid_pos + Vector3(0, 0, 1)):
+			player_grid_pos += Vector3(0, 0, 1)
 	
-func check_tile(pos: Vector2i):
-	if grid[pos.x + ]
+func check_tile(pos: Vector3):
+	var cell = grid_map.get_cell_item(pos)
