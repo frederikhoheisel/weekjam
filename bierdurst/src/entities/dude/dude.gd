da604a31e6ad71841836c08bc6487a04ba59d8b9
i use arch btw <frederik.hoheisel@mailbox.tu-dresden.de>
2026-05-30 19:40:30 +0200

    magnet

--- bierdurst/src/entities/dude/dude.gd
+++ bierdurst/src/entities/dude/dude.gd
@@ -6,6 +6,7 @@ extends Node3D
 @onready var sterni: Node3D = $Node3D/RightArmPivot/sterni
 @onready var animation_player = $AnimationPlayer
 @onready var camera_3d: Camera3D = %Camera3D
+@onready var node_3d: Node3D = $Node3D
 
 var thirsty: bool = true
 var player_here: bool = false
@@ -33,8 +34,21 @@ func _unhandled_input(event: InputEvent) -> void:
 		head.rotation.y = clamp(head.rotation.y, deg_to_rad(-60), deg_to_rad(60))
 
 
+var tween: Tween
+func rotate_drone() -> void:
+	if tween:
+		tween.kill()
+	
+	tween = create_tween()
+	var dir: Vector3 = (drone.global_position - node_3d.global_position).normalized()
+	tween.tween_property(node_3d, "rotate_y", 1.0 - dir.dot(node_3d.global_basis.z), 0.3)
+
+
+
+var drone: Node3D
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if (body.is_in_group("player") && !player_here):
+		drone = body
 		player_here = true
 		GameManager.dude_reached.emit()
 
